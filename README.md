7a39dcec04be7d72033c26c4069c1ca18e1898ea
nicastro <setaloro@hotmail.com>
2018-08-27 22:55:22 +0200

    drag and drop func

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple Drag + Drop with Vanilla JS, based on https://youtube.com/watch?v=C22hQKE_32c
+
+--**nicastro.io**
---
+++ app/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title> Drag+Drop in JS with Vanilla Ice :)</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"> <!-- Not really used anymore for modern IE's -->
+    <meta name="description" content="Some Drag+Drop in JS">
+    <meta name="author" content="Daniele Nicastro">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+<div class="empty">
+    <div class="fill" draggable="true"></div>
+</div>
+<div class="empty"></div>
+<div class="empty"></div>
+<div class="empty"></div>
+<div class="empty"></div>
+
+<script src="js/main.js"></script>
+</body>
+</html>
